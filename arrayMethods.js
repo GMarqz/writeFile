f4e33d463a57726a04a4959b809148279077f2a0
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-03 06:53:09 -0300

    ...

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,7 +1,23 @@
 const characters = require('./characters.json');
+const readline = require('node:readline');
 
+// const newId = '89'
 
+// console.log(Object.keys(characters[0]))
 
+// characters.forEach(character => {
+//     // if(character.id === newId) {
+//     //     console.log('This id already exist.')
+//     // }
+//     console.log(character.id)
+// })
 
-console.log(Object.hasOwn(characters[0], 'name'));
+///Tentei fazer o if ali de cima funcionar, mas sem sucesso. Lembrando que character.id pode ser acessado, só preciso dar um jeito de manipulá-lo.
 
+// console.log(...characters) esse console.log aqui retorna todos os personagens e seus dados pois estamos desestruturando o array do json.
+
+function readByName(obj) {
+    console.log(obj['name'])
+}
+
+readByName(characters[0])
--- idGenerator.js
+++ idGenerator.js
@@ -1,12 +1,26 @@
+const characters = require('./characters.json');
+
 function idGenerator() {
-    const existingId = [89]
-    const random = Math.floor(Math.random() * 1000);
-    if (random == existingId) {
+    const existingId = [];
+
+    for (let i = 0; i < characters.length; i++) {
+        // console.log(characters[i].id)
+        existingId.push(characters[i].id);
+        // console.log(existingId)
+    }
+
+    const random = Math.floor(Math.random() * 10);
+    console.log(random)
+    if (random === existingId) {
         random++
+        console.log('Id já existe, adicionando +1...');
     } else {
         existingId.push(random)
+        console.log('Id não existe, operação realizada com sucesso')
     }
 
 }
+
+idGenerator();
 
 //CONTINUAR ISSO AQUI DEPOIS DE APRENDER A PERCORRER O ARQUIVO JSON E ENCONTRAR SOMENTE A PALAVRA ID, GUARDAR SEU VALOR E GERAR UM NUMERO ALEATORIO QUE NAO SEJA IGUAL AOS IDS EXISTENTES
