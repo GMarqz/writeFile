f0ef6f86f310541e3ee00c58477e661e0c138ab5
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-04 09:08:25 -0300

    Remove old read.js file. Add new function that get characters info by name. readline removed due to an issue that duplicates users input. Functions that rely on readline now have rl as parameter.

--- read.js
+++ read.js
@@ -1,10 +1,37 @@
 const fs = require('fs');
+const characters = require('./characters.json');
 
-function read(){
+function readAll(){
     const dataRead = fs.readFileSync('./characters.json', 'utf8');
     console.log(dataRead);
 }
 
+function readByName(data, value) {
+    return data.find((character) => character['name'] === value);
+}
+
+function getCharacterName(rl) {
+    rl.question(`Enter the characters name: `, characterName => {
+        const characterFound = readByName(characters, characterName);
+        console.log(characterFound);
+        rl.close();
+    })
+}
+
+function read(rl) {
+  rl.question(`Choose an option below: \n[1] - See all characters data \n[2] - See character data by name \n`, option => {
+    if(option === '1') {
+      readAll()
+      rl.close();
+    } else if (option === '2') {
+      getCharacterName(rl)
+    } else {
+      console.log('Invalid option. Please type 1 or 2')
+      rl.close();
+    }
+  });
+}
+
 module.exports = {
-    read: read
+  read: read
 }
