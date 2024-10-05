a48b3b38df1ed0e818681901cf87e14bbbab1899
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-05 07:15:12 -0300

    Finish idGenerator() function
    Remove comments and console.log(). Add return to the function

--- idGenerator.js
+++ idGenerator.js
@@ -1,26 +1,25 @@
 const characters = require('./characters.json');
 
 function idGenerator() {
-    const existingId = [];
+    let existingId = [];
 
     for (let i = 0; i < characters.length; i++) {
-        // console.log(characters[i].id)
         existingId.push(characters[i].id);
-        // console.log(existingId)
+        existingId.toString();
     }
+        
+    let random = Math.floor(Math.random() * 1000);
+ 
+    console.log(random);
 
-    const random = Math.floor(Math.random() * 10);
-    console.log(random)
-    if (random === existingId) {
-        random++
-        console.log('Id já existe, adicionando +1...');
+    if (existingId.includes(random)) {
+        console.log('Id já existe, randomizando novamente...');
+        idGenerator();
     } else {
-        existingId.push(random)
-        console.log('Id não existe, operação realizada com sucesso')
+        return random;
     }
-
 }
 
-idGenerator();
-
-//CONTINUAR ISSO AQUI DEPOIS DE APRENDER A PERCORRER O ARQUIVO JSON E ENCONTRAR SOMENTE A PALAVRA ID, GUARDAR SEU VALOR E GERAR UM NUMERO ALEATORIO QUE NAO SEJA IGUAL AOS IDS EXISTENTES
+module.exports = {
+    idGenerator: idGenerator
+}
