abb6ab75f42ddcd784bbf364f031810468f7d3a2
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-06 15:07:37 -0300

    Refactor code
    - All code related to rl.question is now async
    - Add update function, work with provided name
    - Change some files names (listed down below):
    - mainAppTest.js > app.js / index.js > create.js

---
+++ app.js
@@ -0,0 +1,55 @@
+const readline = require('node:readline/promises');
+const read = require('./read');
+const userInput = require('./userInput');
+const askNewDataInfo = require('./update');
+const characters = require('./characters.json');
+const addDataToJson = require('./create');
+const getCharacterName = require('./read');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+//fsymbols.com for cool fonts like the one below.
+console.log(`
+░██████╗░███████╗███╗░░██╗░██████╗██╗░░██╗██╗███╗░░██╗  ████████╗░█████╗░██╗░░░░░███████╗███╗░░██╗████████╗
+██╔════╝░██╔════╝████╗░██║██╔════╝██║░░██║██║████╗░██║  ╚══██╔══╝██╔══██╗██║░░░░░██╔════╝████╗░██║╚══██╔══╝
+██║░░██╗░█████╗░░██╔██╗██║╚█████╗░███████║██║██╔██╗██║  ░░░██║░░░███████║██║░░░░░█████╗░░██╔██╗██║░░░██║░░░
+██║░░╚██╗██╔══╝░░██║╚████║░╚═══██╗██╔══██║██║██║╚████║  ░░░██║░░░██╔══██║██║░░░░░██╔══╝░░██║╚████║░░░██║░░░
+╚██████╔╝███████╗██║░╚███║██████╔╝██║░░██║██║██║░╚███║  ░░░██║░░░██║░░██║███████╗███████╗██║░╚███║░░░██║░░░
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝  ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░
+
+░█████╗░██╗░░██╗███████╗░█████╗░██╗░░██╗
+██╔══██╗██║░░██║██╔════╝██╔══██╗██║░██╔╝
+██║░░╚═╝███████║█████╗░░██║░░╚═╝█████═╝░
+██║░░██╗██╔══██║██╔══╝░░██║░░██╗██╔═██╗░
+╚█████╔╝██║░░██║███████╗╚█████╔╝██║░╚██╗
+░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝`);
+
+async function initApp() {
+
+    const mainMenu = await rl.question('\n\nWelcome to Genshin Talent Check, what would you like to do today? \n[1] - Add a character \n[2] - Check characters talents \n[3] - Update a character info \n[4] - Delete a character \n[5] - Quit \n(Select the number that matches your desired option): ');
+
+    if(mainMenu === '1'){
+        const firstOption = userInput.userInput(rl);
+    } else if (mainMenu === '2') {
+        console.log('Reading...');
+        await read.read(rl, characters);
+        rl.close();
+    } else if(mainMenu === '3') {
+        console.log('Updating...');
+        const toUpdateCharacter = await getCharacterName.getCharacterName(rl, characters);
+        askNewDataInfo.askNewDataInfo(toUpdateCharacter, rl, addDataToJson);
+    } else if(mainMenu === '4') {
+        console.log('Deleting...') ;
+        rl.close();
+    } else if (mainMenu === '5') {
+        console.log('Leaving...');
+        rl.close();
+    } else {
+        console.log('Sorry, this option is invalid, closing program...');
+        rl.close();
+    }
+}
+
+initApp();
