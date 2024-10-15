a65aa70eac5dc541c1536840be69cedf24a33a37
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-15 18:29:39 -0300

    Implement delete by ID functionality in delete.js

--- delete.js
+++ delete.js
@@ -1,33 +1,12 @@
-const characters = require('./characters.json');
 const { clearJSON } = require('./create');
-const { getCharacterName, readAll } = require('./read');
-const readline = require('node:readline/promises');
 const fs = require('fs');
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-const mualaniForTests =   {
-    "id": 888,
-    "name": "Mualani",
-    "talents": {
-      "normalAtk": 1,
-      "elementalSkill": 1,
-      "elementalBurst": 1,
-      "type": "Contention",
-      "weeklyBossMaterial": "Unknown"
-    },
-    "pic": "...",
-    "description": "Well, this is a test and my current Mualani DOES NOT look exactly like this. Lv80 BTW."
-}
-
-const read = fs.readFileSync('./characters.json') ;
-const readParsed = JSON.parse(read);
-const readArray = [...readParsed, mualaniForTests];
-
 function removeById(id) {
+    
+    const read = fs.readFileSync('./characters.json') ;
+    const readParsed = JSON.parse(read);
+    const readArray = [...readParsed];
+
     let charactersId = [];
     for(let i = 0; i < readArray.length; i++) {
         charactersId.push(readArray[i].id);
@@ -37,10 +16,17 @@ function removeById(id) {
         const indexOfGivenId = charactersId.indexOf(id);
         readArray.splice(indexOfGivenId, 1);
         console.log(`${id} posicionado no index >${indexOfGivenId}< foi removido com sucesso!`);
-        console.log(readArray)
+        console.log(readArray);
+        clearJSON();
+        const returnArrayAsJson = JSON.stringify([...readArray], null, 2)
+        fs.writeFileSync('./characters.json', returnArrayAsJson, (err) => {
+            if (err) throw err;
+        })
     } else {
-        console.log("Deu ruim!")
+        console.log("Unexpected error.")
     }
 } 
 
-removeById(888);
+module.exports = {
+    removeById: removeById
+}
