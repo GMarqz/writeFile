d863a31a560eb57c2a9221bbaf27d936116c3900
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-15 13:59:50 -0300

    Update: function can now remove index by id

--- delete.js
+++ delete.js
@@ -9,29 +9,6 @@ const fs = require('fs');
 //     output: process.stdout
 // });
 
-function deleteCharacter() {
-    let dado = [];
-    const currentCharacters = fs.readFileSync('./characters.json', 'utf-8');
-    // dado = [...currentCharacters];
-    // const parsed = JSON.parse(characters);
-    console.log(typeof characters);
-    // const parsedCurrentCharacters = [...currentCharacters];
-    // currentCharacters.forEach((character) => {
-    //     console.log(character)
-    // });
-}
-
-/////////////////////////////////////////IMPORTANTE!!!!!!!!!/////////////////////////////////////////////
-//OS DADOS LIDOS DO JSON ESTÃO SENDO CONSIDERADOS STRINGS, ATÉ MESMO OS COLCHETES SÃO CONSIDERADOS STRINGS, O QUE SIGNIFICA QUE MÉTODOS ARRAY NÃO FUNCIONAM (EXCETO MÉTODOS DE ARRAYS QUE FUNCIONAM EM STRINGS)
-//ESSE PROVAVELMENTE É A FONTE DOS PROBLEMAS, NÃO ERA PRA SER RETORNADO UMA GRANDE STRING MAS SIM UM ARRAY.
-
-// deleteCharacter();
-
-// console.log(Object.hasOwn(characters, "1"))
-// // console.log(typeof characters[1].name) //string
-// // console.log(typeof characters[1]) //object
-// // console.log(typeof characters) //object
-
 const mualaniForTests =   {
     "id": 888,
     "name": "Mualani",
@@ -47,24 +24,23 @@ const mualaniForTests =   {
 }
 
 const read = fs.readFileSync('./characters.json') ;
-const readParsed = JSON.parse(read)
-// console.log(readParsed)
-const readArray = [...readParsed, mualaniForTests]
-// readArray[19].id = 777
-// console.log(readArray[19])
+const readParsed = JSON.parse(read);
+const readArray = [...readParsed, mualaniForTests];
 
-// for(let i = 0; i < readArray.length; i++) {
-//     console.log(readArray[i].id)
-
-// }
-readArray.filter((character) => {
-    console.log(character.id === mualaniForTests.id)
-    const idIdentificado = character.id === mualaniForTests.id;
-    if(idIdentificado != undefined) {
-        console.log(idIdentificado)
-        const toRemove = readArray.indexOf(idIdentificado)
-        readArray.splice(toRemove, 1)
-        console.log(readArray)
-        return readArray;
+function removeById(id) {
+    let charactersId = [];
+    for(let i = 0; i < readArray.length; i++) {
+        charactersId.push(readArray[i].id);
     }
-})
+    console.log(charactersId);
+    if(charactersId.includes(id)) {
+        const indexOfGivenId = charactersId.indexOf(id);
+        readArray.splice(indexOfGivenId, 1);
+        console.log(`${id} posicionado no index >${indexOfGivenId}< foi removido com sucesso!`);
+        console.log(readArray)
+    } else {
+        console.log("Deu ruim!")
+    }
+} 
+
+removeById(888);
