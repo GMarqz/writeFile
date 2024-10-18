f438058864fde56df274fc55ee89cc569b52ca90
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-18 18:05:15 -0300

    Refactor removeById(): integrate readAll and write()
    Refactor removeById(): integrate readAll() and write() for better JSON handling
    - removeById() now uses readAll() to fetch data and write() to update JSON without the deleted character
    - Removed unnecessary console.log() for character ids

--- delete.js
+++ delete.js
@@ -1,27 +1,22 @@
-const { clearJSON } = require('./create');
-const fs = require('fs');
+const { clearJSON, write } = require('./create');
+const { readAll } = require('./read');
 
 function removeById(id) {
-    
-    const read = fs.readFileSync('./characters.json') ;
-    const readParsed = JSON.parse(read);
-    const readArray = [...readParsed];
+    const readArray = readAll(false);
 
     let charactersId = [];
     for(let i = 0; i < readArray.length; i++) {
         charactersId.push(readArray[i].id);
     }
-    console.log(charactersId);
+    
     if(charactersId.includes(id)) {
         const indexOfGivenId = charactersId.indexOf(id);
         readArray.splice(indexOfGivenId, 1);
         console.log(`${id} posicionado no index >${indexOfGivenId}< foi removido com sucesso!`);
         console.log(readArray);
         clearJSON();
-        const returnArrayAsJson = JSON.stringify([...readArray], null, 2)
-        fs.writeFileSync('./characters.json', returnArrayAsJson, (err) => {
-            if (err) throw err;
-        })
+        const returnArrayAsJson = JSON.stringify([...readArray], null, 2);
+        write(returnArrayAsJson);
     } else {
         console.log("Unexpected error.")
     }
