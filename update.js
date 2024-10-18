f4ce8607d61b006d80e411594ad8dbf7a7492124
MarqzGabrielEstudos <kingvecerett@gmail.com>
2024-10-18 18:03:16 -0300

    Refactor askNewDataInfo(): add readAll and write()
    Refactor askNewDataInfo(): added readAll(false) and write() for data management
    - Integrated readAll(false) to fetch data and write() to update character data in askNewDataInfo()

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const { clearJSON } = require('./create');
+const { clearJSON, write } = require('./create');
+const {readAll} = require('./read');
 
 async function askNewDataInfo(character, rl) {
     const oldLevelMessage = "(or current level in case nothing changed)";
@@ -8,14 +8,6 @@ async function askNewDataInfo(character, rl) {
     const updatedElementalBurstLevel = await rl.question(`Enter the character's new Elemental Burst Level ${oldLevelMessage}: `);
     const updatedDescription = await rl.question(`Enter the character's new Description: `);
 
-    // const read = fs.readFileSync('./characters.json') ;
-    // const readParsed = JSON.parse(read);
-    // const readArray = [...readParsed];
-
-    // let charactersId = [];
-    // for(let i = 0; i < readArray.length; i++) {
-    //     charactersId.push(readArray[i].id);
-    // }
     const updatedCharacter = `
         {
             "id": ${character.id},
@@ -31,12 +23,10 @@ async function askNewDataInfo(character, rl) {
             "description": "${updatedDescription}"
         }
     `
+    console.log(`\n\nCheck your updated data: \n${updatedCharacter}`);
     const updatedCharacterParsed = JSON.parse(updatedCharacter);
-
-    const readJsonData = fs.readFileSync('./characters.json');
     const characterToUpdateId = character.id;
-    const readJsonDataParsed = JSON.parse(readJsonData);
-    const readJsonDataParsedInsideAnArray = [...readJsonDataParsed];
+    const readJsonDataParsedInsideAnArray = readAll(false);
 
     let jsonCharactersId = [];
     for(let i = 0; i < readJsonDataParsedInsideAnArray.length; i++) {
@@ -46,21 +36,13 @@ async function askNewDataInfo(character, rl) {
         const indexOfGivenId = jsonCharactersId.indexOf(characterToUpdateId);
         readJsonDataParsedInsideAnArray.splice(indexOfGivenId, 1, updatedCharacterParsed);
         clearJSON();
-        const sendArrayBackAsJson = JSON.stringify([...readJsonDataParsedInsideAnArray], null, 2)
-        fs.writeFileSync('./characters.json', sendArrayBackAsJson, (err) => {
-            if (err) throw err;
-        })
+        const sendArrayBackAsJson = JSON.stringify([...readJsonDataParsedInsideAnArray], null, 2);
+        write(sendArrayBackAsJson);
     } else {
         console.log("Unexpected error.")
     }
-
-    // console.log(updatedCharacter);
-    // const parsedcharacterUpdated = JSON.parse(updatedCharacter);
-    // const returnUpdatedJson = addDataFunction.addDataToJson(parsedcharacterUpdated);
     rl.close();
 }
-
-// askNewDataInfo(characters[0]);
 
 module.exports = {
     askNewDataInfo: askNewDataInfo
