a8756d623727080681bac3750dd1c84e7cc8ac18
GMarqz <gabrielmarques9782@gmail.com>
2024-10-30 18:27:00 -0300

    Add talentLevel.js

--- talentLevel.js
+++ talentLevel.js
@@ -1,6 +1,10 @@
 const { readAll } = require('./read');
 
+const charactersData = readAll(false);
+const testCharacter = charactersData[18];
+console.log(testCharacter);
 const levels = {
+    0: 0,
     1: 3,
     2: 2,
     3: 4,
@@ -12,10 +16,50 @@ const levels = {
     9: 16
 }
 
-function predictNecessaryMaterials() {
-    const charactersData = readAll(false);
-
-    console.log(charactersData[0].talents.normalAtk)
+function identifyRarity(talentLevel) {
+    if(talentLevel === 1) {
+        return "teachings";
+    } else if(talentLevel >= 2 && talentLevel <= 6) {
+        return "guide";
+    } else if(talentLevel >= 7 && talentLevel <= 10) {
+        return "philosophies";
+    } else {
+        return "invalid level";
+    }
 }
 
-predictNecessaryMaterials();
+function identifyLevel(normalAtkParamms, elementalSkillParamms, elementalBurstParamms, talentTypeParamms) {
+    // const normalAtk = normalAtkParamms;
+    // const elementalSkill = elementalSkillParamms;
+    // const elementalBurst = elementalBurstParamms;
+    const normalAtkLevelIndex = levels[normalAtkParamms];
+    const elementalSkillIndex = levels[elementalSkillParamms];
+    const elementalBurstIndex = levels[elementalBurstParamms];
+    const talentType = talentTypeParamms;
+
+    const normalAtkRarity = identifyRarity(normalAtkParamms);
+    const elementalSkillRarity = identifyRarity(elementalSkillParamms);
+    const elementalBurstRarity = identifyRarity(elementalBurstParamms);
+
+    console.log(`
+        This character needs:
+        \n ${normalAtkLevelIndex} ${normalAtkRarity} of ${talentType} to upgrade its Normal Attack to the next level.
+        \n ${elementalSkillIndex} ${elementalSkillRarity} of ${talentType} to upgrade its Elemental Skill to the next level.
+        \n ${elementalBurstIndex} ${elementalBurstRarity} of ${talentType} to upgrade its Elemental Burst to the next level.
+    `);
+}
+
+function predictNecessaryMaterials(character) {
+    let normalAtkLevel = character.talents.normalAtk;
+    let elementalSkillLevel = character.talents.elementalSkill;
+    let elementalBurstLevel = character.talents.elementalBurst;
+    let characterTalentType = character.talents.type;
+    // const charactersData = readAll(false);
+
+    // console.log(charactersData[0].talents.normalAtk)
+    identifyLevel(normalAtkLevel, elementalSkillLevel, elementalBurstLevel, characterTalentType);
+
+}
+
+predictNecessaryMaterials(testCharacter);
+
