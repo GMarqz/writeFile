f71f1a992f546c8b857ce1057eba767b85329db5
GMarqz <gabrielmarques9782@gmail.com>
2024-11-04 17:45:28 -0300

    change all cjs functions to esm. 100% working

--- talentType.js
+++ talentType.js
@@ -1,15 +1,10 @@
-async function returnAllCharactersWithThisTalentType(rl, charactersData) {
+export default async function returnAllCharactersWithThisTalentType(input, charactersData) {
 
-    const informTalentType = await rl.question('What is the talent type that you wanna check? ');
+    const informTalentType = await input({ message: 'What is the talent type that you wanna check? '});
     const found = charactersData.filter((character) => {
         const characterType = character.talents.type;
        if(characterType === informTalentType) {
         console.log(character);
        }
     });
-
-}
-
-module.exports = {
-    returnAllCharactersWithThisTalentType: returnAllCharactersWithThisTalentType
 }
