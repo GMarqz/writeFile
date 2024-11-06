df7c03f18287dd62f86b2ad40edeef2fc2746481
GMarqz <gabrielmarques9782@gmail.com>
2024-11-06 13:15:25 -0300

    feat: program only accept valid input
    this check if the input is valid for all talent levels

--- handleErrors/userInput.js
+++ handleErrors/userInput.js
@@ -1,14 +1,21 @@
-async function isNormalAtkValid(rl, characterName) {
+async function isThisTalentLevelValid(rl, characterName, talent) {
+    const talentQuestionMessage = `${characterName}'s ${talent} level: `;
+
+    let isValid = false;
     const levelStrings = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    const getCharacterNormalAtk = await rl.question(`${characterName}'s normal attack level: `);
-    if(!levelStrings.includes(getCharacterNormalAtk) || getCharacterNormalAtk > 10 || getCharacterNormalAtk < 1) {
-        console.log("\nERROR: Normal attack level should be a number between 1 and 10\n");
-        isNormalAtkValid(rl, characterName);
-    } else if(levelStrings.includes(getCharacterNormalAtk) && getCharacterNormalAtk >= 1 && getCharacterNormalAtk <= 10) {
-        console.log("Worked")
-    }
+    let getCharacterTalentLevel;
+    while(isValid == false) {
+        //thanks chatgpt for remind me that while() exists.
+        getCharacterTalentLevel = await rl.question(talentQuestionMessage);
+        if(!levelStrings.includes(getCharacterTalentLevel) || getCharacterTalentLevel > 10 || getCharacterTalentLevel < 1) {
+            console.log("\nERROR: Normal attack level should be a number between 1 and 10\n");
+        } else if(levelStrings.includes(getCharacterTalentLevel) && getCharacterTalentLevel >= 1 && getCharacterTalentLevel <= 10) {
+            break;
+        }
+    }    
+    return getCharacterTalentLevel;
 }
 
 module.exports = {
-    isNormalAtkValid: isNormalAtkValid
+    isThisTalentLevelValid: isThisTalentLevelValid
 }
