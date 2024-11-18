ed5a62280a025ac69a9681e85fd65e913357c29d
GMarqz <gabrielmarques9782@gmail.com>
2024-11-18 01:53:08 -0300

    progress on calcTalentMaterialsForSpecificLevel function -incomplete

--- talentLevel.js
+++ talentLevel.js
@@ -15,7 +15,7 @@ const levels = [
 //Ask the user: Whats the maximum level you wanna invest for this talent?
 // currentLevel + nextLevels until maximumLevel.
 
-function calcTalentMaterialsForSpecificLevel(currentLevel, specificLevel) {
+function calcTalentMaterialsForSpecificLevel(currentLevel, specificLevel, talentMaterialType, talentSkill) {
 
     const talentLevels = [
         0,
@@ -31,24 +31,43 @@ function calcTalentMaterialsForSpecificLevel(currentLevel, specificLevel) {
         16
     ];
 
-    let allGivenLevels = [];
-    let sum = 0;
+    let guideLevels = [];
+    let philosophiesLevels = [];
+    let sumGuide = 0;
+    let sumPhilo = 0;
 
-    while(currentLevel <= specificLevel){
-        let clevel = talentLevels[currentLevel];
-        allGivenLevels.push(clevel);
+    while(currentLevel <= 2) {
         currentLevel++;
-        console.log(currentLevel);
-        console.log(allGivenLevels);
+        // console.log('You need 3 teachings');
     }
 
-    for(let i = 0; i < allGivenLevels.length; i++) {
-        sum += allGivenLevels[i];
-        console.log(sum)
+    while(currentLevel <= specificLevel && currentLevel >= 3 && currentLevel <= 6){
+        let guideLevel = talentLevels[currentLevel];
+        guideLevels.push(guideLevel);
+        currentLevel++;
+        // console.log(guideLevels + ' \nEste foi o guide levels')
     }
+
+    while(currentLevel >= 7 && currentLevel <= specificLevel) {
+        let philosophiesLevel = talentLevels[currentLevel];
+        philosophiesLevels.push(philosophiesLevel);
+        currentLevel++;
+        // console.log(philosophiesLevels + '\n Este foi o philo levels');
+    }
+
+    for(let i = 0; i < guideLevels.length; i++) {
+        sumGuide += guideLevels[i];
+        // console.log(sumGuide + 'esta é a soma do guide')
+    }
+    for(let i = 0; i < philosophiesLevels.length; i++) {
+        sumPhilo += philosophiesLevels[i];
+        // console.log(sumPhilo + 'esta é a soma do philo')
+    }
+
+    console.log(`This character needs ${sumGuide} guides of ${talentMaterialType} and ${sumPhilo} philosophies of ${talentMaterialType} to upgrade its ${talentSkill} to your desired level.`);
 }
 
-calcTalentMaterialsForSpecificLevel(6, 9);
+// calcTalentMaterialsForSpecificLevel(1, 10, 'Gold', 'Elemental Skill');
 
 function identifyRarity(talentLevel) {
     if(talentLevel === 1) {
@@ -102,5 +121,6 @@ function predictNecessaryMaterials(character) {
 }
 
 module.exports = {
-    predictNecessaryMaterials: predictNecessaryMaterials
+    predictNecessaryMaterials: predictNecessaryMaterials,
+    calcTalentMaterialsForSpecificLevel: calcTalentMaterialsForSpecificLevel
 }
